fe9023b7a1cf208c6a055ec4522c5cd371e090fa
aconn1994 <adam.m.connolly94@gmail.com>
2024-01-08 18:41:30 -0500

    save 1/8/24

---
+++ Input/FA-18C_hornet/joystick/Saitek Pro Flight X-56 Rhino Stick {326F0C20-6D25-11ee-800E-444553540000}.diff.lua
@@ -0,0 +1,79 @@
+local diff = {
+	["axisDiffs"] = {
+		["a3043cd13"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Throttle Designator Controller - VERTICAL AXIS",
+		},
+		["a3044cd13"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RX",
+				},
+			},
+			["name"] = "Throttle Designator Controller - HORIZONTAL AXIS",
+		},
+	},
+	["keyDiffs"] = {
+		["d3002pnilu3002cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Gun Trigger - SECOND DETENT (Press to shoot)",
+		},
+		["d3003pnilu3003cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Weapon Release Button",
+		},
+		["d3009pnilu3009cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Select Sparrow",
+		},
+		["d3010pnilu3010cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Select Gun",
+		},
+		["d3011pnilu3011cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Select AMRAAM",
+		},
+		["d3012pnilu3012cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Select Sidewinder",
+		},
+		["d3013pnilu3013cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "Undesignate/Nose Wheel Steer Switch",
+		},
+	},
+}
+return diff
---
+++ Input/FA-18C_hornet/joystick/Saitek Pro Flight X-56 Rhino Throttle {326C7410-6D25-11ee-8008-444553540000}.diff.lua
@@ -0,0 +1,135 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["name"] = "Pitch",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Y",
+				},
+			},
+		},
+		["a2002cdnil"] = {
+			["name"] = "Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+		["a2005cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Thrust Left",
+		},
+		["a2006cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Thrust Right",
+		},
+		["a3043cd13"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RX",
+				},
+			},
+			["name"] = "Throttle Designator Controller - VERTICAL AXIS",
+		},
+		["a3044cd13"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Throttle Designator Controller - HORIZONTAL AXIS",
+		},
+	},
+	["keyDiffs"] = {
+		["d3017pnilunilcd2vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "FLAP Switch - Down",
+		},
+		["d3017pnilunilcd2vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "FLAP Switch - Up",
+		},
+		["d3031pnilu3031cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Cage/Uncage Button",
+		},
+		["d3032pnilu3032cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Dispense Switch - Forward(CHAFF)/Center(OFF)",
+		},
+		["d3033pnilu3033cd13vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "Dispense Switch - Aft(FLARE)/Center(OFF)",
+		},
+		["d3035pnilu3035cd13vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN29",
+				},
+			},
+			["name"] = "Speed Brake Switch - EXTEND",
+		},
+		["d3035pnilunilcd13vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "Speed Brake Switch - RETRACT",
+		},
+		["d430pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Landing Gear Control Handle - UP",
+		},
+		["d431pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Landing Gear Control Handle - DOWN",
+		},
+	},
+}
+return diff
